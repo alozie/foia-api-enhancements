6bc9317b841a4f0c13dd1bb2c237548818f6735c
Alozie Nwosu <alozie.nwosu@acquia.com>
2019-09-24 19:14:17 -0400

    FOIA-117: Adds auto-calculation for fields on sections V.A. and XII.B.

--- docroot/modules/custom/foia_advcalc/js/advcalc-field.js
+++ docroot/modules/custom/foia_advcalc/js/advcalc-field.js
@@ -2,7 +2,6 @@
   Drupal.behaviors.advcalc_field = {
       attach: function attach() {
 
-
           // Fields from sections IX and X to calculate overall_x_perc_costs.
           $("#edit-field-overall-ix-proc-costs-0-value, #edit-field-overall-x-total-fees-0-value").change(function() {
               var overall_ix_proc_costs = Number($("#edit-field-overall-ix-proc-costs-0-value").val());
@@ -13,7 +12,6 @@
                   $('#edit-field-overall-x-perc-costs-0-value').val(overall_x_perc_costs);
               }
           });
-
 
           // Fields from section VI A to calculate app_pend_start_yr.
           var via = $('input[id^="edit-field-admin-app-via"]');
@@ -48,9 +46,77 @@
 
           });
 
-          // // Fields from section V A to calculate field_req_pend_end_yr.
-          // // Fields from XII B to calculate field_pend_end_yr.
-          // // Fields from IX and X to calculate field_perc_costs per agency.
+          // Section V A automatically calculate field_req_pend_end_yr.
+          // req_pend_start_yr + req_received_yr - req_processed_yr = req_pend_end_yr
+          var via = $('input[id^="edit-field-foia-requests-va"]');
+          via.change(function() {
+              var va_count = $("table[id^='field-foia-requests-va-values'] tbody" +
+                  " tr");
+              var va_vals = [];
+
+              for (var i = 0; i < va_count.length; i++) {
+                  var edit_pend_start_name = "[data-drupal-selector='edit-field-foia-requests-va-" + i + "-subform-field-req-pend-start-yr-0-value'";
+                  var edit_rec_name = "[data-drupal-selector='edit-field-foia-requests-va-" + i + "-subform-field-req-received-yr-0-value'";
+                  var edit_proc_name = "[data-drupal-selector='edit-field-foia-requests-va-" + i + "-subform-field-req-processed-yr-0-value'";
+                  var edit_pend_end_name = "[data-drupal-selector='edit-field-foia-requests-va-" + i + "-subform-field-req-pend-end-yr-0-value'";
+                  va_vals[i] = {
+                      appPendStartYr: Number($(edit_pend_start_name).val()),
+                      appReceivedYr: Number($(edit_rec_name).val()),
+                      appProcessedYr: Number($(edit_proc_name).val()),
+                      appPendEndYr: function() {
+                          return this.appPendStartYr + this.appReceivedYr - this.appProcessedYr;
+                      }
+                  };
+                  $(edit_pend_end_name).val(va_vals[i].appPendEndYr());
+              }
+
+              $("#edit-field-overall-req-pend-start-yr-0-value, #edit-field-overall-req-received-yr-0-value, #edit-field-overall-req-processed-yr-0-value").change(function() {
+                  var overall_app_pend_start_yr = Number($("#edit-field-overall-req-pend-start-yr-0-value").val());
+                  var overall_app_received_yr = Number($("#edit-field-overall-req-received-yr-0-value").val());
+                  var overall_app_processed_yr = Number($("#edit-field-overall-req-processed-yr-0-value").val());
+                  var overall_app_pend_end_yr = overall_app_pend_start_yr + overall_app_received_yr - overall_app_processed_yr;
+                  $('#edit-field-overall-req-pend-end-yr-0-value').val(overall_app_pend_end_yr);
+              });
+
+          });
+
+          // Section XII B automatically calculate field_pend_end_yr.
+          // pend_start_yr + con_during_yr - proc_start_yr = pend_end_yr
+          var xiib = $('input[id^="edit-field-foia-xiib"]');
+          xiib.change(function() {
+              var xiib_count = $("table[id^='field-foia-xiib-values'] tbody" +
+                  " tr");
+              var xiib_vals = [];
+
+              for (var i = 0; i < xiib_count.length; i++) {
+                  var edit_pend_start_name = "[data-drupal-selector='edit-field-foia-xiib-" + i + "-subform-field-pend-start-yr-0-value'";
+                  var edit_rec_name = "[data-drupal-selector='edit-field-foia-xiib-" + i + "-subform-field-con-during-yr-0-value'";
+                  var edit_proc_name = "[data-drupal-selector='edit-field-foia-xiib-" + i + "-subform-field-proc-start-yr-0-value'";
+                  var edit_pend_end_name = "[data-drupal-selector='edit-field-foia-xiib-" + i + "-subform-field-pend-end-yr-0-value'";
+                  xiib_vals[i] = {
+                      appPendStartYr: Number($(edit_pend_start_name).val()),
+                      appReceivedYr: Number($(edit_rec_name).val()),
+                      appProcessedYr: Number($(edit_proc_name).val()),
+                      appPendEndYr: function() {
+                          return this.appPendStartYr + this.appReceivedYr - this.appProcessedYr;
+                      }
+                  };
+                  $(edit_pend_end_name).val(xiib_vals[i].appPendEndYr());
+              }
+
+              $("#edit-field-overall-xiib-pend-start-yr-0-value, #edit-field-overall-xiib-con-during-yr-0-value, #edit-field-overall-xiib-proc-start-yr-0-value").change(function() {
+                  var overall_pend_start_yr = Number($("#edit-field-overall-xiib-pend-start-yr-0-value").val());
+                  var overall_con_during_yr = Number($("#edit-field-overall-xiib-con-during-yr-0-value").val());
+                  var overall_processed_yr = Number($("#edit-field-overall-xiib-proc-start-yr-0-value").val());
+                  var overall_pend_end_yr = overall_pend_start_yr + overall_con_during_yr - overall_processed_yr;
+                  $('#edit-field-overall-xiib-pend-end-yr-0-value').val(overall_pend_end_yr);
+              });
+
+          });
+
+          // Fields from IX and X to calculate field_perc_costs per agency.
+          //FOIA Personnel and Costs IX. proc_costs / Fees X. total_fees  = Fees X. perc_costs
+
 
       }
   }
